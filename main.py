e236f9a445383109d6e816218f08bb375f30c956
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2022-11-01 23:32:10 +0100

    Add some colours and debug mode

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import serial
 import sqlite3
+#import pyreadline3
+
+#readline.set_auto_history(True)
+verbose = False
 
 sql_con = sqlite3.connect("data.db")
 sql_cursor = sql_con.cursor()
 sql_cursor.execute("CREATE TABLE IF NOT EXISTS cards(hash PRIMARY KEY, credit)")
 
+def blue(str): return f"\033[34m{str}\033[0m"
+def red(str): return f"\033[31m{str}\033[0m"
+def yellow(str): return f"\033[93m{str}\033[0m"
+def gray(str): return f"\033[90m{str}\033[0m"
+def green(str): return f"\033[32m{str}\033[0m"
 
 def sql(query):
-    #print(query)
+    if verbose: print(gray(query))
     res = sql_cursor.execute(query)
     sql_con.commit()
     return res
@@ -37,10 +46,15 @@ def setcredit(uid,credit):
     res = sql(f"UPDATE cards SET credit = {credit} WHERE hash='{uid}'")    
 
 while True:
-    print("> ", end='')
+    print(blue("> "), end='')
+    
     rawinput = input().split()
+    if len(rawinput) < 1:
+        print()
+        continue
 
     cmd = rawinput.pop(0)
+
     args = rawinput
 
     match cmd:
@@ -48,11 +62,12 @@ while True:
             uid = get_rfid()
             credit = getcredit(uid)
             print(f"{uid}: {credit} credit")
+
         case "add":
             try: 
                 amount = int(args[0])
             except:
-                print("No valid amount")
+                print(yellow("Not a valid amount"))
                 continue
 
             uid = get_rfid()
@@ -65,14 +80,21 @@ while True:
             try: 
                 amount = int(args[0])
             except:
-                print("No valid amount")
+                print(yellow("Not a valid amount"))
                 continue
 
             uid = get_rfid()
             credit = getcredit(uid)
             if credit < amount:
-                print("Not enough credit")
+                print(yellow("Not enough credit"))
                 continue
             credit -= amount
             setcredit(uid,credit)
             print(f"New credit: {credit}")
+
+        case "debug":
+            verbose = not verbose
+            print(gray(f"Debuggin is {('on' if verbose else 'off')}"))
+
+        case other:
+            print("Unknown command")
