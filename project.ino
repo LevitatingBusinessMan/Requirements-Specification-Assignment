57e7c25e7d3fa0ac7203d1612f820ecfdec66ca7
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2022-11-02 18:10:46 +0100

    Arduino: use same buffor for the block

--- project.ino
+++ project.ino
@@ -8,8 +8,7 @@ RFID rfid;
 uint8_t key[KEYLENGTH] = {0xd7, 0xe7, 0xeb, 0x9e, 0x4c, 0xce, 0x25, 0x43, 0x62, 0x91, 0x1b, 0xdd, 0x3d, 0xf2, 0x16, 0xb2};
 
 AES128 aes = AES128();
-uint8_t inbuf[16] = {0};
-uint8_t outbuf[16] = {0};
+uint8_t block[16] = {0};
 
 void setup() {
   Serial.begin(9600);
@@ -25,15 +24,15 @@ void loop() {
 
   uint32_t uid = rfid.readUID();
 
-  inbuf[0] = uid;
-  inbuf[1] = uid >> 8;
-  inbuf[2] = uid >> 16;
-  inbuf[3] = uid >> 24;
+  block[0] = uid;
+  block[1] = uid >> 8;
+  block[2] = uid >> 16;
+  block[3] = uid >> 24;
 
-  memcpy(inbuf + 4, canary, 4);
+  memcpy(block + 4, canary, 4);
 
-  aes.encryptBlock(outbuf,inbuf);
+  aes.encryptBlock(block,block);
 
-  Serial.write(outbuf,16);
+  Serial.write(block,16);
   Serial.flush();
 }
