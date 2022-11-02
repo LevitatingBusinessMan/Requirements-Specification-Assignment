0901e99b7054e2173cdb7d318597fee335a66a68
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2022-11-02 16:27:39 +0100

    Encryption and stuff

--- project.ino
+++ project.ino
@@ -1,31 +1,36 @@
-#include <MD5.h>
-#include <SPI.h>
-#include <rfid.h>
+#include <AES.h>
 #include <MFRC522.h>
-
-#define SALT 0xb00b
+#include <rfid.h>
+#define KEYLENGTH 16
 
 RFID rfid;
+
+uint8_t key[KEYLENGTH] = {0xd7, 0xe7, 0xeb, 0x9e, 0x4c, 0xce, 0x25, 0x43, 0x62, 0x91, 0x1b, 0xdd, 0x3d, 0xf2, 0x16, 0xb2};
+
+AES128 aes = AES128();
+uint8_t inbuf[16] = {0};
+uint8_t outbuf[16] = {0};
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
   rfid.begin();
+  aes.setKey(key, KEYLENGTH);
 }
 
 void loop() {
   uint32_t uid = rfid.readUID();
   
-  char buf[10];
-  itoa(uid, buf, 16);
+  //char buf[10];
+  //itoa(uid, buf, 16);
 
-  buf[9] = SALT;
+  inbuf[0] = uid;
+  inbuf[1] = uid >> 8;
+  inbuf[2] = uid >> 16;
+  inbuf[3] = uid >> 24;
+  
+  aes.encryptBlock(outbuf,inbuf);
 
-  unsigned char* hash = MD5::make_hash(buf, 10);
-  char *md5str = MD5::make_digest(hash, 16);
-  Serial.println(md5str);
-
-  free(hash);
-  free(md5str);
+  Serial.write(outbuf,16);
 
 }
